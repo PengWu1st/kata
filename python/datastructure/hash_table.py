2936d0a0423fb8a83a37aa980543e0b7098d01a4
Peng Wu <wupeng1127@foxmail.com>
2024-08-13 06:27:07 +0000

    implement hash table

---
+++ python/datastructure/hash_table.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+from single_linked_list import SingleLinkedList
+
+class HashNode:
+
+    def __init__(self, key, value = None) -> None:
+        self.key, self.value = key, value
+
+    def __eq__(self, other: HashNode) -> bool:
+        return self.key == other.key
+
+class HashTable:
+    """
+    Example 1
+    >>> hash_table = HashTable()
+    >>> hash_table.insert("key1", "value1")
+    >>> hash_table.insert("key2", "value2")
+    >>> hash_table.find("key1")
+    'value1'
+    >>> hash_table.find("key2")
+    'value2'
+    >>> hash_table.remove("key1")
+    >>> hash_table.find("key1") is None
+    True
+
+    """
+
+    def __init__(self, size: int = 10) -> None:
+        self._size = size
+        self._table = [SingleLinkedList() for _ in range(self._size)]
+
+    def insert(self, key, value):
+        index = self._hash(key)
+        self._table[index].insert(HashNode(key, value))
+    
+    def _hash(self, key):
+        return hash(key) % self._size
+
+    def find(self, key):
+        index = self._hash(key)
+        node = self._table[index].find(HashNode(key))
+        if node:
+            return node.value
+        return None
+
+    def remove(self, key):
+        index = self._hash(key)
+        self._table[index].remove(HashNode(key))
--- python/datastructure/single_linked_list.py
+++ python/datastructure/single_linked_list.py
@@ -16,8 +16,14 @@ class SingleLinkedList:
     Example 1
     >>> sll = SingleLinkedList()
     >>> sll.insert(1)
+    >>> 1 in sll
+    True
     >>> sll.find(1)
-    Node(1)
+    1
+    >>> sll.remove(1)
+    >>> sll.find(1)
+    
+
     """
 
     def __init__(self) -> None:
@@ -30,9 +36,29 @@ class SingleLinkedList:
     
     def find(self, val: Any) -> Node | None:
         p = self.head
-        
         while p:
             if p.val == val:
-                return p
+                return p.val
             p = p.next
     
+    def remove(self, val: Any):
+        p = self.head
+        prev = None
+        while p:
+            if p.val == val:
+                if prev:
+                    prev.next = p.next
+                else:
+                    self.head = p.next
+                return 
+            prev = p
+            p = p.next
+            
+
+    def __contains__(self, val: Any) -> bool:
+        p = self.head
+        while p:
+            if p.val == val:
+                return True
+            p = p.next
+        return False
