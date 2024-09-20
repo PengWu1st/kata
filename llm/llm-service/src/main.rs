bcaa57ba078e0660d377ae1ecd95f1d13652d1e3
Peng Wu <wupeng1127@foxmail.com>
2024-09-20 10:15:33 +0000

    simplest example

--- llm/llm-service/src/main.rs
+++ llm/llm-service/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let auth = Auth::from_env().unwrap();
     let openai = OpenAI::new(auth, "https://api.openai.com/v1/");
     let body = ChatBody {
-        model: "gpt-3.5-turbo".to_string(),
+        model: "gpt-4o-mini".to_string(),
         max_tokens: Some(7),
         temperature: Some(0_f32),
         top_p: Some(0_f32),
@@ -26,5 +26,6 @@ fn main() {
     let rs = openai.chat_completion_create(&body);
     let choice = rs.unwrap().choices;
     let message = &choice[0].message.as_ref().unwrap();
+    println!("{}",message.content);
     assert!(message.content.contains("Hello"));
 }
