96f012171e02422478f2e3f070686178ca07fc0c
Peng Wu <wupeng1127@foxmail.com>
2024-09-20 10:45:23 +0000

    introduce axum as web framwork

--- llm/llm-service/src/main.rs
+++ llm/llm-service/src/main.rs
@@ -1,7 +1,11 @@
 use openai_api_rust::chat::*;
 use openai_api_rust::*;
+use axum::{
+    routing::get,
+    Router,
+};
 
-fn main() {
+async fn hello() -> String {
     // Load API key from environment OPENAI_API_KEY.
     // You can also hadcode through `Auth::new(<your_api_key>)`, but it is not recommended.
     let auth = Auth::from_env().unwrap();
@@ -28,4 +32,18 @@ fn main() {
     let message = &choice[0].message.as_ref().unwrap();
     println!("{}",message.content);
     assert!(message.content.contains("Hello"));
+    message.content.clone()
+}
+
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/hello", get(hello));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
