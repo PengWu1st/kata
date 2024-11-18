28dfba37e8601fec36995dad41c0bae1fe4fbaf2
warren-wupeng <warren.wu@visionwx.com>
2024-11-18 12:09:06 +0800

    feat: 添加右侧二分查找函数及其单元测试，增强二分查找功能

--- data-structure-and-algorithm/binary_search.py
+++ data-structure-and-algorithm/binary_search.py
@@ -30,6 +30,21 @@ def binary_search_left(arr: list, value):
     return -1
 
 
+def binary_search_right(arr: list, value):
+    l, r = 0, len(arr) - 1
+    while l <= r:
+        m = (l + r) // 2
+        if arr[m] < value:
+            l = m + 1
+        elif arr[m] > value:
+            r = m - 1
+        else:
+            if m == len(arr) - 1 or arr[m + 1] != value:
+                return m
+            l = m + 1
+    return -1
+
+
 def lower_bound(arr: list, value):
     # find index i, such that arr[i] >= value
     l, r = 0, len(arr)
@@ -62,6 +77,14 @@ class TestBinarySearch(unittest.TestCase):
         self.assertEqual(binary_search(arr, 1), 0)
         self.assertEqual(binary_search(arr, 9), 8)
         self.assertEqual(binary_search(arr, 10), -1)
+
+    def test_binary_search_left(self):
+        arr = [1, 2, 2, 2, 5, 6, 7, 8, 9]
+        self.assertEqual(binary_search_left(arr, 2), 1)
+
+    def test_binary_search_right(self):
+        arr = [1, 2, 2, 2, 5, 6, 7, 8, 9]
+        self.assertEqual(binary_search_right(arr, 2), 3)
 
     def test_lower_bound(self):
         arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
