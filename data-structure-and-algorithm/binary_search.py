498175515cc93b75f3e132131d298f32b531f4fc
warren-wupeng <warren.wu@visionwx.com>
2024-11-18 11:49:57 +0800

    feat: 添加左侧二分查找和下界查找函数，增强二分查找功能及其单元测试

--- data-structure-and-algorithm/binary_search.py
+++ data-structure-and-algorithm/binary_search.py
@@ -15,6 +15,33 @@ def binary_search(arr: list, value):
     return -1
 
 
+def binary_search_left(arr: list, value):
+    l, r = 0, len(arr) - 1
+    while l <= r:
+        m = (l + r) // 2
+        if arr[m] < value:
+            l = m + 1
+        elif arr[m] > value:
+            r = m - 1
+        else:
+            if m == 0 or arr[m - 1] != value:
+                return m
+            r = m - 1
+    return -1
+
+
+def lower_bound(arr: list, value):
+    # find index i, such that arr[i] >= value
+    l, r = 0, len(arr)
+    while l < r:
+        m = (l + r) // 2
+        if arr[m] < value:
+            l = m + 1
+        else:
+            r = m
+    return l
+
+
 class TestBinarySearch(unittest.TestCase):
 
     def test_binary_search(self):
@@ -23,3 +50,14 @@ class TestBinarySearch(unittest.TestCase):
         self.assertEqual(binary_search(arr, 1), 0)
         self.assertEqual(binary_search(arr, 9), 8)
         self.assertEqual(binary_search(arr, 10), -1)
+
+    def test_lower_bound(self):
+        arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.assertEqual(lower_bound(arr, 5), 4)
+        self.assertEqual(lower_bound(arr, 1), 0)
+        self.assertEqual(lower_bound(arr, 9), 8)
+        self.assertEqual(lower_bound(arr, 10), 9)
+
+    def test_lower_bound_2(self):
+        arr = [1, 2, 2, 2, 5, 6, 7, 8, 9]
+        self.assertEqual(lower_bound(arr, 2), 1)
